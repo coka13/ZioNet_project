d147b6af652f8959932d5b025d18adfd34a0dc50
coka13 <isafeze@gmail.com>
2024-06-28 22:17:48 +0300

    fixed queue connection and smtp sending

--- DBservice/entrypoint.sh
+++ DBservice/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Waiting for RabbitMQ to be ready..."
+/app/wait-for-it.sh rabbitmq:5672 --timeout=60 --strict -- echo "RabbitMQ is up"
+
 echo "Initializing service1..."
 node createDB.js  
 
--- DBservice/lib/mailer/sendMail.js
+++ DBservice/lib/mailer/sendMail.js
@@ -4,7 +4,7 @@ import nl2br from "nl2br"
 
 
 let mailer = nodemailer.createTransport({
-    host: "127.0.0.1",  // can add smtp host
+    host: "smtp4dev",  // can add smtp host
     port: 25,           // can add port for smtp
     secure: false,
 })
@@ -12,7 +12,7 @@ let mailer = nodemailer.createTransport({
 export const  sendMail = async ( email )=>{
     await mailer.sendMail({
         from: 'admin@admin.com', 
-        to: "userr@gmail.com",
+        to: "user@gmail.com",
         subject: "Action completed", 
         text: email,
         html:`<h1>Action completed</h1><p>${nl2br(email)}</p>`
